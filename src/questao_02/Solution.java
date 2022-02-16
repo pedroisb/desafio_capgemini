8130708270e99ccae62b4031df66fe449b4d8e11
Pedro Brito <pedroisb28@gmail.com>
2022-02-16 15:16:49 -0300

    challeng 02 done. pending tests and exceptions

--- src/questao_02/Solution.java
+++ src/questao_02/Solution.java
@@ -12,18 +12,35 @@ package questao_02;
 // Débora digitou uma string aleatória no campo de senha, porém ela não tem certeza se é uma senha forte. Para ajudar Débora, construa um algoritmo que informe qual é o número mínimo de caracteres que devem ser adicionados para uma string qualquer ser considerada segura.
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Solution {
-
-    // ler input
-    // buscar por certos caracteres no input
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        //BufferedReader?
-        
+
         String password = scanner.nextLine();
+        int charsNeeded = 0;
+        int charsMissing = 6 - password.length();
+
+        List<Pattern> regexes= new ArrayList<Pattern>();
+        regexes.add(Pattern.compile(".*[A-Z].*"));
+        regexes.add(Pattern.compile(".*[a-z].*"));
+        regexes.add(Pattern.compile(".*\\d.*"));
+        regexes.add(Pattern.compile(".*[!@#$%^&*()-+].*"));
+
+        for (Pattern regex : regexes) {
+            if (!regex.matcher(password).matches())
+                charsNeeded++;
+        }
+
+        if (charsMissing > charsNeeded)
+            charsNeeded = charsMissing;
+
+        System.out.println(charsNeeded);
     }
 }
