8186d56da5e9aa3c906a4abc2ac542a9fdc07f35
Pedro Brito <pedroisb28@gmail.com>
2022-02-16 17:18:38 -0300

    third challenge done. pending only tests with junit

--- src/questao_01/Solution.java
+++ src/questao_01/Solution.java
@@ -1,9 +1,5 @@
 package questao_01;
 
-
-//Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.
-
-
 import java.util.Scanner;
 
 public class Solution {
@@ -11,12 +7,14 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
+
+        // 1) criada variavel a para armazenar string "*"
         String a = "*";
 
+        // 2) bloco de código deverá ser iterado n vezes, correspondente ao número informado pelo usuário. ao fim de cada iteração, será adicionado um "*" à String a
+        // 2.1) formatei a mensagem a ser exibida para alinhar os asteriscos à direita, como consta da imagem do documento contendo o desafio. para isso, determinei que cada string a ser exibida deveria ter um número de caracteres n, correspondente ao inteiro informado pelo usuário
         for (int i = 1; i <= n ; i++) {
-
             System.out.printf("%"+n+"s%n", a);
             a += "*";
         }
--- src/questao_02/Solution.java
+++ src/questao_02/Solution.java
@@ -1,17 +1,5 @@
 package questao_02;
 
-
-//Débora se inscreveu em uma rede social para se manter em contato com seus amigos. A página de cadastro exigia o preenchimento dos campos de nome e senha, porém a senha precisa ser forte. O site considera uma senha forte quando ela satisfaz os seguintes critérios:
-
-//        ● Possui no mínimo 6 caracteres.          -> 6 char
-//        ● Contém no mínimo 1 digito.              -> 1 int
-//        ● Contém no mínimo 1 letra em minúsculo.  -> 1 char lowerCase
-//        ● Contém no mínimo 1 letra em maiúsculo.  -> 1 char upperCase
-//        ● Contém no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+    -> 1 char dentre estes
-
-// Débora digitou uma string aleatória no campo de senha, porém ela não tem certeza se é uma senha forte. Para ajudar Débora, construa um algoritmo que informe qual é o número mínimo de caracteres que devem ser adicionados para uma string qualquer ser considerada segura.
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,25 +10,29 @@ public class Solution {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         String password = scanner.nextLine();
-        int charsNeeded = 0;
-        int charsMissing = 6 - password.length();
 
+        // 1) criei variaveis para identificar o numero de caracteres necessarios para alcançar 6 e outra para receber o numero de caracteres necessários para atender todos os requisitos
+        int charsMissing = 6 - password.length();
+        int requiredChars = 0;
+
+        // 2) criei um arraylist com 4 regexes, correspondentes aos requisitos exigidos pela senha: letra maiúscula, letra minúscula, digito e caractere especial
         List<Pattern> regexes= new ArrayList<Pattern>();
         regexes.add(Pattern.compile(".*[A-Z].*"));
         regexes.add(Pattern.compile(".*[a-z].*"));
         regexes.add(Pattern.compile(".*\\d.*"));
-        regexes.add(Pattern.compile(".*[!@#$%^&*()-+].*"));
+        regexes.add(Pattern.compile(".*[!@#$%^&*()\\-+].*"));
 
+        // 3) percorri os elementos do array de regexes para buscar correspondência destes com a senha informada pelo usuário. a cada falta de correspondência, será adicionado 1 ao número de caracteres requeridos
         for (Pattern regex : regexes) {
             if (!regex.matcher(password).matches())
-                charsNeeded++;
+                requiredChars++;
         }
 
-        if (charsMissing > charsNeeded)
-            charsNeeded = charsMissing;
+        // 4) por fim, caso o número de caracteres faltantes para alcançar 6 seja superior ao número de caracteres requeridos para atender os requisitos específicos da senha até então, o número de caracteres requeridos passará a ser aquele armanezado em caracteres faltantes
+        if (charsMissing > requiredChars)
+            requiredChars = charsMissing;
 
-        System.out.println(charsNeeded);
+        System.out.println(requiredChars);
     }
 }
